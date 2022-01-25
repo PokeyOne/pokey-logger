4d93bb66e98675550ce5babbca2ad4416e8e88bc
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-25 12:48:27 -0800

    existing log handler method implementations

--- src/existing_log_handler.rs
+++ src/existing_log_handler.rs
@@ -1,3 +1,13 @@
+//! This module contains the different methods and handlers for what to do
+//! when a log file already exists.
+
+#[cfg(test)]
+mod tests;
+
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use serde::{Deserialize, Serialize};
 
 /// The method of handling a pre-existing log file when starting a new session.
@@ -14,5 +24,92 @@ pub enum ExistingLogHandler {
 impl Default for ExistingLogHandler {
     fn default() -> Self {
         ExistingLogHandler::Append
+    }
+}
+
+#[derive(Debug)]
+pub enum ExistingLogHandlerOpenError {
+    /// Something went wrong when opening the existing log file or creating a new one.
+    Io(io::Error),
+    /// The path could not be used as a log file. This could because the path
+    /// is not a file.
+    InvalidPath
+}
+
+impl From<io::Error> for ExistingLogHandlerOpenError {
+    fn from(error: io::Error) -> Self {
+        ExistingLogHandlerOpenError::Io(error)
+    }
+}
+
+impl ExistingLogHandler {
+    pub fn open_file<P: AsRef<Path>>(&self, path: P) -> Result<File, ExistingLogHandlerOpenError> {
+        match self {
+            ExistingLogHandler::Append => if path.as_ref().exists(){
+                debug!("Appending to existing log file");
+                match File::options().append(true).open(path) {
+                    Ok(file) => Ok(file),
+                    Err(e) => Err(ExistingLogHandlerOpenError::Io(e))
+                }
+            } else {
+                debug!("Creating new log file");
+                match File::create(path) {
+                    Ok(file) => Ok(file),
+                    Err(e) => Err(ExistingLogHandlerOpenError::Io(e))
+                }
+            },
+            // TODO: Overwrite should handle new file creation.
+            ExistingLogHandler::Overwrite => match File::create(path) {
+                Ok(file) => Ok(file),
+                Err(e) => Err(ExistingLogHandlerOpenError::Io(e))
+            },
+            ExistingLogHandler::Rename => {
+                let path_buf = PathBuf::from(path.as_ref());
+                if path_buf.exists() {
+                    // Find the existing extension
+                    let existing_extension = match path_buf.extension() {
+                        Some(extension) => format!(".{}", extension.to_string_lossy()),
+                        None => String::new()
+                    };
+
+                    // Add the date before the extension
+                    let new_path = path_buf.with_extension(
+                        &format!("{}{}",
+                                 chrono::Local::now().format("%Y-%m-%d_%H-%M-%S"),
+                                 existing_extension));
+
+                    // Rename the file
+                    let mut new_file = match File::create(new_path.as_path()) {
+                        Ok(file) => file,
+                        Err(err) => return Err(ExistingLogHandlerOpenError::Io(err))
+                    };
+
+                    // Write the old file to the new file
+                    let old_file_content = match std::fs::read_to_string(path_buf.as_path()) {
+                        Ok(content) => content,
+                        Err(err) => return Err(ExistingLogHandlerOpenError::Io(err))
+                    };
+                    match new_file.write_all(old_file_content.as_bytes()) {
+                        Ok(_) => (),
+                        Err(err) => return Err(ExistingLogHandlerOpenError::Io(err))
+                    };
+
+                    let res = File::options()
+                        .create(true)
+                        .write(true)
+                        .open(path_buf.as_path());
+
+                    match res {
+                        Ok(file) => Ok(file),
+                        Err(err) => Err(ExistingLogHandlerOpenError::Io(err))
+                    }
+                } else {
+                    match File::create(path) {
+                        Ok(file) => Ok(file),
+                        Err(err) => Err(ExistingLogHandlerOpenError::Io(err))
+                    }
+                }
+            }
+        }
     }
 }
---
+++ src/existing_log_handler/tests.rs
@@ -0,0 +1,88 @@
+use super::*;
+use tempfile::tempdir;
+
+#[test]
+fn test_append() -> Result<(), ExistingLogHandlerOpenError> {
+    let dir = tempdir()?;
+    assert!(dir.path().exists());
+
+    let log_path = dir.path().join("test.log");
+    debug!("log_path: {:?}", log_path);
+
+    let mut file = ExistingLogHandler::Append.open_file(log_path.as_path())?;
+    assert!(log_path.exists());
+    let mut expected_len = file.write(b"Hello, world!")?;
+    drop(file);
+
+    // Re-open the file and write another message
+    let mut file = ExistingLogHandler::Append.open_file(log_path.as_path())?;
+    expected_len += file.write(b"Goodbye, world!")?;
+    assert_ne!(0, expected_len);
+    drop(file);
+
+    // Check the file contents
+    let actual_len = std::fs::read_to_string(log_path)?.as_bytes().len();
+    assert_eq!(expected_len, actual_len);
+
+    Ok(())
+}
+
+#[test]
+fn test_overwrite() -> Result<(), ExistingLogHandlerOpenError> {
+    let dir = tempdir()?;
+    assert!(dir.path().exists());
+
+    let log_path = dir.path().join("test.log");
+    debug!("log_path: {:?}", log_path);
+
+    let mut file = ExistingLogHandler::Overwrite.open_file(log_path.as_path())?;
+    assert!(log_path.exists());
+    assert_ne!(0, file.write(b"Hello, world!")?);
+    drop(file);
+
+    // Re-open the file and write another message
+    let mut file = ExistingLogHandler::Overwrite.open_file(log_path.as_path())?;
+    let expected_len = file.write(b"Goodbye, world!")?;
+    assert_ne!(0, expected_len);
+    drop(file);
+
+    // Check the file contents
+    let actual_len = std::fs::read_to_string(log_path)?.as_bytes().len();
+    assert_eq!(expected_len, actual_len);
+
+    Ok(())
+}
+
+#[test]
+fn test_rename() -> Result<(), ExistingLogHandlerOpenError> {
+    let dir = tempdir()?;
+    assert!(dir.path().exists());
+
+    let log_path = dir.path().join("test.log");
+    debug!("log_path: {:?}", log_path);
+
+    let mut file = ExistingLogHandler::Rename.open_file(log_path.as_path())?;
+    assert!(log_path.exists());
+    assert_ne!(0, file.write(b"Hello, world!")?);
+    drop(file);
+
+    // Re-open the file and write another message
+    let mut file = ExistingLogHandler::Rename.open_file(log_path.as_path())?;
+    let expected_len = file.write(b"Goodbye, world!")?;
+    assert_ne!(0, expected_len);
+    drop(file);
+
+    // The file should not be overwritten and only have the new message
+    let actual_text = std::fs::read_to_string(log_path)?;
+    let actual_len = actual_text.as_bytes().len();
+    assert_eq!(expected_len, actual_len, "actual_text: {:?}", actual_text);
+
+    // There should be a new file with the original name with a date/time stamp
+    let dir_entries: Vec<String> = std::fs::read_dir(dir.path())?
+        .map(|entry| entry.unwrap().path().file_name().unwrap().to_string_lossy().to_string())
+        .collect();
+    assert_eq!(2, dir_entries.len());
+    debug!("dir_entries: {:?}", dir_entries);
+
+    Ok(())
+}
