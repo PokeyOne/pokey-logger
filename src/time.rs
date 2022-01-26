ee75d1af3e03133324dd5e24c0aa151c39034067
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-26 01:41:42 -0800

    update current_time_box documentation

--- src/time.rs
+++ src/time.rs
@@ -7,7 +7,11 @@ use chrono::prelude::*;
 
 /// Gets the string to write to the log already formatted to be printed.
 ///
-/// An example might be: "[18:14:09]"
+/// An example might be: "[18:14:09]" (using the default format). A custom
+/// format can be provided using the same syntax as what chrono offers in its
+/// [`chrono::format::strftime`]
+///
+/// The default format string is "%H:%M:%S"
 pub fn current_time_box(format_string: Option<String>) -> String {
     let format_string = match format_string {
         Some(fs) => fs,
